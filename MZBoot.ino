1219f64e70e203f1c1490b52640305b56d8a1775
Matt Jenkins <matt@majenko.co.uk>
2018-02-12 22:18:35 +0000

    Broken out configs into separate files

--- MZBoot.ino
+++ MZBoot.ino
@@ -1,58 +1,30 @@
-#pragma config FMIIEN = ON              // Ethernet RMII/MII Enable (MII Enabled)
-#pragma config FETHIO = ON              // Ethernet I/O Pin Select (Default Ethernet I/O)
-#pragma config PGL1WAY = OFF            // Permission Group Lock One Way Configuration (Allow multiple reconfigurations)
-#pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
-#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
-#pragma config FUSBIDIO = OFF           // USB USBID Selection (Controlled by Port Function)
-
-// DEVCFG2
-#pragma config FPLLIDIV = DIV_3         // System PLL Input Divider (1x Divider)
-#pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
-#pragma config FPLLICLK = PLL_POSC      // System PLL Input Clock Selection (FRC is input to the System PLL)
-#pragma config FPLLMULT = MUL_50        // System PLL Multiplier (PLL Multiply by 50)
-#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (2x Divider)
-#pragma config UPLLFSEL = FREQ_24MHZ    // USB PLL Input Frequency Selection (USB PLL input is 24 MHz)
-
-// DEVCFG1
-#pragma config FNOSC = SPLL             // Oscillator Selection Bits (System PLL)
-#pragma config DMTINTV = WIN_127_128    // DMT Count Window Interval (Window/Interval value is 127/128 counter value)
-#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Enable SOSC)
-#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
-#pragma config POSCMOD = EC             // Primary Oscillator Configuration (External clock mode)
-#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Enabled)
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disabled, FSCM Disabled)
-#pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
-#pragma config WDTSPGM = STOP           // Watchdog Timer Stop During Flash Programming (WDT stops during Flash programming)
-#pragma config WINDIS = NORMAL          // Watchdog Timer Window Mode (Watchdog Timer is in non-Window mode)
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
-#pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window size is 25%)
-#pragma config DMTCNT = DMT31           // Deadman Timer Count Selection (2^31 (2147483648))
-#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
-
-// DEVCFG0
-#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
-#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
-#pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (Communicate on PGEC2/PGED2)
-#pragma config TRCEN = ON               // Trace Enable (Trace features in the CPU are enabled)
-#pragma config BOOTISA = MIPS32         // Boot ISA Selection (Boot code and Exception code is MIPS32)
-#pragma config FECCCON = OFF_UNLOCKED   // Dynamic Flash ECC Configuration (ECC and Dynamic ECC are disabled (ECCCON bits are writable))
-#pragma config FSLEEP = OFF             // Flash Sleep Mode (Flash is powered down when the device is in Sleep mode)
-#pragma config DBGPER = ALLOW_PG2       // Debug Mode CPU Access Permission (Allow CPU access to Permission Group 2 permission regions)
-#pragma config SMCLR = MCLR_NORM        // Soft Master Clear Enable bit (MCLR pin generates a normal system Reset)
-#pragma config SOSCGAIN = GAIN_2X       // Secondary Oscillator Gain Control bits (2x gain setting)
-#pragma config SOSCBOOST = OFF          // Secondary Oscillator Boost Kick Start Enable bit (Normal start of the oscillator)
-#pragma config POSCGAIN = GAIN_1X       // Primary Oscillator Gain Control bits (1x gain setting)
-#pragma config POSCBOOST = ON           // Primary Oscillator Boost Kick Start Enable bit (Boost the kick start of the oscillator)
-#pragma config EJTAGBEN = NORMAL        // EJTAG Boot (Normal EJTAG functionality)
-
-// DEVCP0
-#pragma config CP = OFF                 // Code Protect (Protection Disabled)
-
 #include <Flash.h>
 
-USBHS usbDriver;
+
+#if defined(_BOARD_PKOB_DA_)
+#include "configs/pkob-da.h"
+#elif defined(_BOARD_MOD_BASE_FX_)
+#include "configs/mod-base-fx.h"
+#else
+#error "There is no configuration for your board"
+#endif
+
+
+
+#define ENABLE_DEBUG 0
+#define BOOT_TIMEOUT_SECONDS 5
+
+
+USBFS usbDriver;
 USBManager USB(usbDriver, 0x04d8, 0x0f5f, "chipKIT", "HID Bootloader");
 HID_Raw HID;
+
+#if (ENABLE_DEBUG == 1)
+CDCACM debugSerial;
+#define DEBUG(...) debugSerial.printf(__VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
 
 volatile uint8_t packet[64];
 volatile bool packetValid = false;
@@ -269,7 +241,12 @@ void processAN1388Packet(uint8_t *data, uint32_t len) {
 }
 
 void setup() {
+    delay(100);
+    
     USB.addDevice(HID);
+#if (ENABLE_DEBUG == 1)
+    USB.addDevice(debugSerial);
+#endif
     HID.onOutputReport(outputReport);
     USB.begin();
 }
@@ -278,18 +255,19 @@ void loop() {
 
     static uint32_t ts = millis();
 
-    if (millis() - ts > 5000) {
+    if (millis() - ts > (BOOT_TIMEOUT_SECONDS * 1000)) {
         executeApp();
     }
     
     if (packetValid) {
+        DEBUG("Packet valid\r\n");
         ts = millis();
         if (packetLength >= 3) {
             uint16_t cs = (packet[packetLength - 1] << 8) | packet[packetLength - 2];
             uint16_t newcs = calculateCRC16((uint8_t *)packet, packetLength-2);
+            DEBUG("Checksum %02x => %02x\r\n", cs, newcs);
 
             if (cs == newcs) {
-
                 processAN1388Packet((uint8_t *)packet, packetLength - 2);
             }
         }       
